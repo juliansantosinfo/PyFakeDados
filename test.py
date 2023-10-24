9dfa413c84d40f9f887360b4f22ba0b5dda4af32
Julian Santos <julian.santos.info@gmail.com>
2023-10-23 22:56:09 -0300

    test: Implemented test for Bairro.

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from PyFakeDados.bairro import gerar_bairro
+from PyFakeDados.cep import gerar_cep
+from PyFakeDados.cnpj import gerar_cnpj, validar_cnpj
+from PyFakeDados.cpf import gerar_cpf, validar_cpf
+from PyFakeDados.ctps import gerar_ctps
+
+class TestBairro(unittest.TestCase):
+    
+    def test_gerar_bairro(self):
+        result = gerar_bairro()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+
+if __name__ == '__main__':
+    unittest.main()
