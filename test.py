ed37bbbe69a34df933bd1bb9e8d066f15592bacb
Julian de Almeida Santos <julian.santos.info@gmail.com>
2024-04-28 13:26:56 -0300

    test: Implemented test for email, empresa, nome, endereco and estado.

--- test.py
+++ test.py
@@ -5,6 +5,11 @@ from PyFakeDados.cep import gerar_cep
 from PyFakeDados.cnpj import gerar_cnpj, validar_cnpj
 from PyFakeDados.cpf import gerar_cpf, validar_cpf
 from PyFakeDados.ctps import gerar_ctps
+from PyFakeDados.email import gerar_email, gerar_email_empresa, gerar_email_pessoa
+from PyFakeDados.empresa import gerar_segmento, gerar_nome_empresa, gerar_empresa
+from PyFakeDados.nome import gerar_nome
+from PyFakeDados.endereco import gerar_endereco
+from PyFakeDados.estado import gerar_uf, validar_uf, gerar_estado, busca_nome_uf
 
 class TestBairro(unittest.TestCase):
     
@@ -13,12 +18,14 @@ class TestBairro(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertIsInstance(result, str)
 
+
 class TestCEP(unittest.TestCase):
     
     def test_gerar_cep(self):
         result = gerar_cep()
         self.assertIsNotNone(result)
         self.assertIsInstance(result, str)
+
 
 class TestCNPJ(unittest.TestCase):
     
@@ -33,6 +40,7 @@ class TestCNPJ(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertIsInstance(result, bool)
 
+
 class TestCPF(unittest.TestCase):
 
     def test_gerar_cpf(self):
@@ -46,12 +54,89 @@ class TestCPF(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertIsInstance(result, bool)
 
+
 class TestCTPS(unittest.TestCase):
     
     def test_gerar_ctps(self):
         result = gerar_ctps()
         self.assertIsNotNone(result)
         self.assertIsInstance(result, str)
+
+
+class TestEmail(unittest.TestCase):
+    
+    def test_gerar_email(self):
+        result = gerar_email(gerar_nome())
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+
+    def test_gerar_email_empresa(self):
+        result = gerar_email_empresa(gerar_nome_empresa())
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+
+    def test_gerar_email_pessoa(self):
+        result = gerar_email_pessoa(gerar_nome())
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+
+
+class TestEmpresa(unittest.TestCase):
+    
+
+    def test_gerar_segmento(self):
+        result = gerar_segmento()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+    
+
+    def test_gerar_nome_empresa(self):
+        result = gerar_nome_empresa()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+    
+
+    def test_gerar_empresa(self):
+        result = gerar_empresa()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, dict)
+
+
+class TestEndereco(unittest.TestCase):
+
+
+    def test_gerar_endereco(self):
+        result = gerar_endereco()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, dict)
+
+
+class TestEstado(unittest.TestCase):
+
+
+    def test_gerar_uf(self):
+        result = gerar_uf()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+    
+
+    def test_validar_uf(self):
+        result = validar_uf(gerar_uf())
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, bool)
+    
+
+    def test_gerar_estado(self):
+        result = gerar_estado()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+    
+
+    def test_busca_nome_uf(self):
+        result = busca_nome_uf(gerar_uf())
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result, str)
+
 
 if __name__ == '__main__':
     unittest.main()
